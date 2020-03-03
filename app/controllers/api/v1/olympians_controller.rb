80997c39f2c36318384c05f63cc52856b55f3919
John Travers <johnk.travers@gmail.com>
2020-03-03 13:11:12 -0700

    Add conditional for age parameter to olympians controller index action

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::OlympiansController < ApplicationController
   def index
-    olympians = Olympian.includes(:team, :sport, :olympian_events)
+    if params[:age]
+      if params[:age] == 'youngest'
+        olympians = Olympian.youngest_olympians.includes(:team, :sport, :olympian_events)
+      else
+        error = "Invalid request. Please enter either 'youngest' or 'oldest' for the age parameter."
+        return render json: Error.new(error).json, status: 400
+      end
+    else
+      olympians = Olympian.includes(:team, :sport, :olympian_events)
+    end
+
     serialized_olympians = OlympianSerializer.new(olympians).serialized_json
     formatted_olympians = OlympianFormatter.new(serialized_olympians).json_response
     render json: formatted_olympians
