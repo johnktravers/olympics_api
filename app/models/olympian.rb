c682ac709375559a07293c97f25fa1d29f47b129
John Travers <johnk.travers@gmail.com>
2020-03-03 12:04:54 -0700

    Add total_medals_won instance method to olympian model with test

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -14,4 +14,8 @@ class Olympian < ApplicationRecord
   belongs_to :sport
   has_many :olympian_events
   has_many :events, through: :olympian_events
+
+  def total_medals_won
+    olympian_events.where('medal > 0').count
+  end
 end
