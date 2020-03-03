7d0d2c78e95ecdaa559f378bd29ecf0139a10fc4
John Travers <johnk.travers@gmail.com>
2020-03-02 17:43:22 -0700

    Allow nil for olympian height and weight attributes

--- spec/models/olympian_spec.rb
+++ spec/models/olympian_spec.rb
@@ -7,17 +7,19 @@ RSpec.describe Olympian, type: :model do
 
     it { should validate_presence_of :age }
     it { should validate_presence_of :sex }
-    it { should validate_presence_of :height }
-    it { should validate_presence_of :weight }
+    it { should validate_presence_of(:height).allow_nil }
+    it { should validate_presence_of(:weight).allow_nil }
 
     it { should validate_inclusion_of(:sex).in_array(['M', 'F']) }
 
     it { should validate_numericality_of(:age).only_integer }
     it { should validate_numericality_of(:age).is_greater_than(0) }
 
+    it { should validate_numericality_of(:height).allow_nil }
     it { should validate_numericality_of(:height).only_integer }
     it { should validate_numericality_of(:height).is_greater_than(0) }
 
+    it { should validate_numericality_of(:weight).allow_nil }
     it { should validate_numericality_of(:weight).only_integer }
     it { should validate_numericality_of(:weight).is_greater_than(0) }
   end
