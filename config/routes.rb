f6666f523a60cd14ae40992e161b5f35a77692d6
John Travers <johnk.travers@gmail.com>
2020-03-03 18:31:31 -0700

    Add namespaced route for event medalists endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/olympians',      to: 'olympians#index'
-      get '/olympian_stats', to: 'olympian_stats#show'
-      get '/events',         to: 'events#index'
+      get '/olympians',            to: 'olympians#index'
+      get '/olympian_stats',       to: 'olympian_stats#show'
+      get '/events',               to: 'events#index'
+      get '/events/:id/medalists', to: 'events#show'
     end
   end
 end
