4d6871dd02f42065062c2ff7970db36458468463
John Travers <johnk.travers@gmail.com>
2020-03-03 18:32:27 -0700

    Add show action to events controller to show an event and its medalists

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,4 +4,21 @@ class Api::V1::EventsController < ApplicationController
     formatted_events = EventFormatter.new(sports).json_response
     render json: formatted_events
   end
+
+  def show
+    event = Event.find_by(id: params[:id])
+    if event
+      medalists = Olympian
+        .includes(:team)
+        .joins(:olympian_events)
+        .where('olympian_events.medal > 0 AND olympian_events.event_id = ?', event.id)
+        .order('olympian_events.medal DESC, olympians.id')
+
+      formatted_medalists = MedalistFormatter.new(event, medalists).json_response
+      render json: formatted_medalists
+    else
+      error = 'Invalid event ID. Please try again with an integer between 1 and 305.'
+      return render json: Error.new(error).json, status: 400
+    end
+  end
 end
